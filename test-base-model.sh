d9ec6d3ad15ad38f031e23d3b02ea38fdd8fa553
luminolous <syauqinabil276@gmail.com>
2026-03-10 14:34:47 +0700

    chore: change test file destination folder

--- test-base-model.sh
+++ test-base-model.sh
@@ -1,4 +1,4 @@
-python test-base-model-output.py \
+python test/test-base-model-output.py \
   --repo-root /workspace/DreamBooth-CLoRA \
   --config configs/tasks_5char.yaml \
   --task-index 0 \
--- test-ccip.sh
+++ test-ccip.sh
@@ -1,3 +1,3 @@
-python test-ccip.py \
+python test/test-ccip.py \
   --repo-root /workspace/DreamBooth-CLoRA \
   --config configs/tasks_5char.yaml
